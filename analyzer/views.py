f9f6b907587383d72bd5a6f7f9c58141208158e7
yash114bansal <yash114bansal@gmail.com>
2024-06-27 19:17:39 +0530

    add views to analyze urine strip

--- analyzer/views.py
+++ analyzer/views.py
@@ -1,4 +1,26 @@
 from django.shortcuts import render
 
-def index(request):
+from rest_framework import status
+from rest_framework.generics import GenericAPIView
+from rest_framework.response import Response
+
+from .utils import analyze_urine_strip
+from .serializers import ImageUploadSerializer
+
+def index(request): 
     return render(request, 'index.html')
+
+class ImageUploadView(GenericAPIView):
+    """
+    API Endpoint to get urine analysis test
+    """
+    serializer_class = ImageUploadSerializer
+    
+    def post(self, request):
+
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        
+        image = serializer.validated_data["image"]
+        colors = analyze_urine_strip(image)
+        return Response(colors, status=status.HTTP_200_OK)
